70ae8d8831e0ef153bf21398d9553edbf02edbc8
Victor Henrique <victorhenrique121413@gmail.com>
2020-06-04 15:54:51 -0300

    Utilizando template engine e instalação do nunjucks

--- src/server.js
+++ src/server.js
@@ -2,12 +2,19 @@ const express = require("express")
 const server = express()
 
 
+// configurar a pasta publica
+server.use(express.static("public"))
+
 // configurar caminhos na minha aplicação 
 // pagina inicial
 // req: Requisição
 // res: Resposta
 server.get("/", (req, res) => {
-    
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
 })
 
 // ligar o servidor
