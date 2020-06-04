c2e7ccc5bee6b8d205e11a526c8e57c6503504a9
Victor Henrique <victorhenrique121413@gmail.com>
2020-06-04 15:31:26 -0300

    Pagina inicial criada e instalação do nodemon

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 const express = require("express")
 const server = express()
 
+
+// configurar caminhos na minha aplicação 
+// pagina inicial
+// req: Requisição
+// res: Resposta
+server.get("/", (req, res) => {
+    
+})
+
 // ligar o servidor
 server.listen(3000)
